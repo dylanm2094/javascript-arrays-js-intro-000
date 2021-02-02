fcc1ac14c4595fee47b5738575aa7ede1d8406f5
Dylan Mann <dylanm2094@gmail.com>
2021-02-02 19:24:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,13 @@ var chocolateBars = [
   'hundred grand',
   'kitkat',
   'skittles']
+
+function addElementToBeginningOfArray(array,n){
+  var array2 = [n,...array]
+  return array2
+}
+
+function destructivelyAddElementToBeginningOfArray(array,n){
+  array.unshift(n)
+  return array
+}
